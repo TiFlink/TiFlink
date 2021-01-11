ee49efedd5294741f033a5c260793a612678399f
chase <yun.er.run@gmail.com>
2021-01-11 22:21:03 +0900

    Implement RegionCDCClient and partially finished CDCClient

--- client/src/main/java/org/tikv/cdc/CDCClient.java
+++ client/src/main/java/org/tikv/cdc/CDCClient.java
@@ -1,6 +1,105 @@
 package org.tikv.cdc;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tikv.cdc.RegionCDCClient.RegionCDCClientBuilder;
+import org.tikv.common.TiConfiguration;
+import org.tikv.common.region.TiRegion;
+import org.tikv.common.util.ConcreteBackOffer;
+import org.tikv.kvproto.Cdcpb.ChangeDataEvent;
+import org.tikv.kvproto.Cdcpb.Event;
+import org.tikv.kvproto.Cdcpb.Event.Row;
+
 public class CDCClient implements AutoCloseable {
-    public void close() {
+    private final TiConfiguration conf;
+    private final RegionCDCClientBuilder clientBuilder;
+    private final Logger logger = LoggerFactory.getLogger(CDCClient.class);
+    private final TreeMap<TiRegion, RegionCDCClient> regionClients;
+    private final PriorityQueue<RegionState> regionStates;
+
+    public CDCClient(final TiConfiguration conf, final RegionCDCClientBuilder clientBuilder,
+            final List<TiRegion> regions, final long startTs) {
+        this.conf = conf;
+        this.clientBuilder = clientBuilder; 
+        this.regionClients = new TreeMap<>((a, b) -> a.getRowEndKey().compareTo(b.getRowEndKey()));
+        this.regionStates = new PriorityQueue<>(
+            regions.stream()
+                .map((r) -> RegionState.create(r, startTs))
+                .collect(Collectors.toList())
+        );
+    }
+
+    synchronized public void close() {
+        for (RegionCDCClient client : regionClients.values()) {
+            try {
+              client.close();
+            } catch (final Exception e) {
+                logger.error("failed to close RegionCDCClient", e);
+            }
+        }
+    }
+
+    synchronized public List<Row> pullRows() {
+        if (regionStates.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        final RegionState next = regionStates.poll();
+        final RegionCDCClient client = regionClients.computeIfAbsent(next.region, clientBuilder::build);
+        final ChangeDataEvent cdcEvent = client.getChangeDataEvent(ConcreteBackOffer.newGetBackOff(), next.timestamp);
+
+        final List<Event> result = cdcEvent.getEventsList();
+        // TODO: implement this method
+        return Collections.emptyList();
+    }
+
+    synchronized public long getTimestamp() {
+        return regionStates.stream().mapToLong(r -> r.timestamp).min().orElse(0);
+    }
+
+    synchronized public long getResolvedTs() {
+        return regionStates.stream().mapToLong(r -> r.timestamp).min().orElse(0);
+    }
+
+    static private class RegionState implements Comparable<RegionState> {
+        final TiRegion region;
+        final long timestamp;
+        final long resolvedTs;
+
+        private RegionState(final TiRegion region, final long timestamp) {
+            this(region, timestamp, timestamp);
+        }
+
+        private RegionState(final TiRegion region, final long timestamp, final long resolvedTs) {
+            this.region = region;
+            this.timestamp = Math.max(timestamp, resolvedTs);
+            this.resolvedTs = resolvedTs;
+        }
+
+        public RegionState with(final long timestamp, final long resolvedTs) {
+            return new RegionState(region, timestamp, resolvedTs);
+        }
+
+        public RegionState withTimestamp(final long timestamp) {
+            return new RegionState(region, timestamp, resolvedTs);
+        }
+
+        public RegionState withResolvedTs(final long resolvedTs) {
+            return new RegionState(region, timestamp, resolvedTs);
+        }
+
+        static public RegionState create(final TiRegion region, final long timestamp) {
+            return new RegionState(region, timestamp);
+        }
+
+        @Override
+        public int compareTo(final RegionState that) {
+            return Long.compare(this.timestamp, that.timestamp);
+        }
     }
 }
--- client/src/main/java/org/tikv/cdc/RegionCDCClient.java
+++ client/src/main/java/org/tikv/cdc/RegionCDCClient.java
@@ -14,8 +14,51 @@ import org.tikv.common.region.RegionManager;
 import org.tikv.common.region.TiRegion;
 import org.tikv.common.util.BackOffer;
 import org.tikv.common.util.ChannelFactory;
+import org.tikv.common.util.ConcreteBackOffer;
 
 public class RegionCDCClient extends AbstractGRPCClient<ChangeDataBlockingStub, ChangeDataStub> {
+    private final TiRegion region;
+    private final ChangeDataBlockingStub blockingStub;
+    private final ChangeDataStub asyncStub;
+
+    private RegionCDCClient(
+            final TiConfiguration conf,
+            final ChannelFactory channelFactory,
+            final ChangeDataBlockingStub blockingStub,
+            final ChangeDataStub asyncStub,
+            final TiRegion region
+    ) {
+        super(conf, channelFactory);
+        this.blockingStub = blockingStub;
+        this.asyncStub = asyncStub;
+        this.region = region;
+    }
+
+    public ChangeDataEvent getChangeDataEvent(final BackOffer backOffer,final long startTs) {
+        final ChangeDataRequest request = ChangeDataRequest.newBuilder()
+            .setRegionId(region.getId())
+            .setCheckpointTs(startTs)
+            .setStartKey(region.getStartKey())
+            .setEndKey(region.getEndKey())
+            .setRegionEpoch(region.getRegionEpoch())
+            .build();
+        return callWithRetry(backOffer, ChangeDataGrpc.getEventFeedMethod(), () -> request, null);
+    }
+
+    @Override
+    protected ChangeDataBlockingStub getBlockingStub() {
+        return blockingStub;
+    }
+
+    @Override
+    protected ChangeDataStub getAsyncStub() {
+        return asyncStub;
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+
     static public class RegionCDCClientBuilder {
         final TiConfiguration conf;
         final ChannelFactory channelFactory;
@@ -34,58 +77,16 @@ public class RegionCDCClient extends AbstractGRPCClient<ChangeDataBlockingStub, 
             this.pdClient = pdClient;
         }
 
-        public RegionCDCClient build(final Store store, final long regionId) {
+        public RegionCDCClient build(final TiRegion region) {
+            final Store store = regionManager.getStoreById(region.getLeader().getStoreId());
             final ManagedChannel channel = channelFactory.getChannel(store.getAddress());
             final ChangeDataBlockingStub blockingStub = ChangeDataGrpc.newBlockingStub(channel);
             final ChangeDataStub asyncStub = ChangeDataGrpc.newStub(channel);
-            return new RegionCDCClient(conf, channelFactory, blockingStub, asyncStub, regionId);
+            return new RegionCDCClient(conf, channelFactory, blockingStub, asyncStub, region);
         }
 
-        public RegionCDCClient build(final long storeId, final long regionId) {
-            return build(regionManager.getStoreById(storeId), regionId);
+        public RegionCDCClient build(final long regionId) {
+            return build(pdClient.getRegionByID(ConcreteBackOffer.newGetBackOff(), regionId));
         }
-
-        public RegionCDCClient build(final TiRegion region) {
-            return build(region.getLeader().getStoreId(), region.getId());
-        }
-    }
-
-    private final long regionId;
-    private final ChangeDataBlockingStub blockingStub;
-    private final ChangeDataStub asyncStub;
-
-    private RegionCDCClient(
-            final TiConfiguration conf,
-            final ChannelFactory channelFactory,
-            final ChangeDataBlockingStub blockingStub,
-            final ChangeDataStub asyncStub,
-            final long regionId
-    ) {
-        super(conf, channelFactory);
-        this.blockingStub = blockingStub;
-        this.asyncStub = asyncStub;
-        this.regionId = regionId;
-    }
-
-    public ChangeDataEvent getChangeDataEvent(final BackOffer backOffer,final long startTs) {
-        final ChangeDataRequest request = ChangeDataRequest.newBuilder()
-            .setRegionId(regionId)
-            .setCheckpointTs(startTs)
-            .build();
-        return callWithRetry(backOffer, ChangeDataGrpc.getEventFeedMethod(), () -> request, null);
-    }
-
-    @Override
-    protected ChangeDataBlockingStub getBlockingStub() {
-        return blockingStub;
-    }
-
-    @Override
-    protected ChangeDataStub getAsyncStub() {
-        return asyncStub;
-    }
-
-    @Override
-    public void close() throws Exception {
     }
 }
